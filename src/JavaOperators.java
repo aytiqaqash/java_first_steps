62acbe38cc6eef80dd1e69353e129761a5820046
aytiqaqash <aytiqaqash@gmail.com>
2022-01-15 20:12:23 -0500

    JavaOperators

---
+++ src/JavaOperators.java
@@ -0,0 +1,23 @@
+public class JavaOperators {
+
+//Objective: To understand the concept of Java operators.
+//'Problem Description: Create a Java class as shown in the example and change the program to use compound assignments like "+="
+
+        public static void main (String[] args) {
+            int res = 1 + 2; // result is now 3
+            System.out.println (res);
+            res= res - 1; // result is now 2
+            System.out.println (res);
+            res = res * 2; // result is now 4
+            System.out.println(res);
+            res = res / 2; // result is now 2
+            System.out.println(res);
+            res = res + 8; // result is now 10
+            System.out.println(res);
+            res = res % 7; // result is now 3
+            System.out.println(res);
+        }
+
+
+
+}
