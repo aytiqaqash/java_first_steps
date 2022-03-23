5c323caaab9c234443974d7c02550ef8743bbe06
aytiqaqash <aytiqaqash@gmail.com>
2022-03-22 21:12:25 -0400

    Decryption without key

--- src/CaesarCipher/CaesarCipher.java
+++ src/CaesarCipher/CaesarCipher.java
@@ -1,6 +1,10 @@
 package CaesarCipher;
 
+import java.util.Arrays;
+
 public class CaesarCipher {
+
+    // Encryption
     public String encrypt(String input, int key){
 
 
@@ -45,8 +49,6 @@ public class CaesarCipher {
        return encrypted.toString();
     }
     public String encryptWithTwoKey(String input, int key1, int key2){
-
-
 //        0. Make StringBuilder to work with Strings (encrypted)
         StringBuilder encrypted = new StringBuilder(input);
 
@@ -59,8 +61,6 @@ public class CaesarCipher {
         String shiftedAlphabet2 = alphabet.substring(key2) + alphabet.substring(0,key2);
         String shiftedLowerAlphabet1 = lowerAlphabet.substring(key1) + lowerAlphabet.substring(0,key1);
         String shiftedLowerAlphabet2 = lowerAlphabet.substring(key2) + lowerAlphabet.substring(0,key2);
-
-
 
 //        3. Count from 0 to < message.length,  (i)
        for (int i = 0 ; i < encrypted.length(); i++){
@@ -102,17 +102,61 @@ public class CaesarCipher {
        return encrypted.toString();
     }
 
+    // Decryption Without Keys
+    public int[] countLetters(String message){
+        // alphabet
+        String alph = "abcdefghijklmnopqrstuvwxyz";
+        // array for each char of alphabet
+        int[] counts = new int[26];
+        // Go through the message chars and add counts for that char
+        for (int k = 0; k<message.length();k++){
+            char ch = Character.toLowerCase(message.charAt(k));
+            int dex =alph.indexOf(ch);
+            if (dex!=-1){
+                counts[dex] += 1;
+            }
+        }
+        return counts;
+    }
+
+    // Most Frequent char index
+    public int maxIndex(int[] vals){
+        int maxDex = 0;
+        for (int k=0; k<vals.length;k++){
+            if(vals[k] >= vals[maxDex]){
+                maxDex = k;
+            }
+        }
+        return maxDex;
+    }
+
+    public String decrypt (String encrypted){
+        // create an CaesarCipher object
+        CaesarCipher cc = new CaesarCipher();
+
+        // get the Frequency of the letters
+        int[] freqs = countLetters(encrypted);
+        System.out.println("freqs " + Arrays.toString(freqs));
+
+        // get the index of the most frequent char
+        int maxDex = maxIndex(freqs);
+        System.out.println("maxDex " + maxDex);
+
+        int dkey = maxDex-4;
+        if(maxDex<4){
+            dkey = 26 - (4-maxDex);
+        }
+        System.out.println("dKey " + dkey);
+
+        return cc.encrypt(encrypted,26-dkey);
+    }
+
     public static void main(String[] args) {
         CaesarCipher caesarCipher = new CaesarCipher();
-
-        //encryption
-        System.out.println(caesarCipher.encrypt("I am",17));
-
-        //decryption
-        System.out.println(caesarCipher.encrypt("I am ",26-17));
-
-        //encryption with 2 Keys
-        System.out.println(caesarCipher.encryptWithTwoKey("First Legion",23,17));
+        String message = "When trying to decrypt a cipher text based on a substitution cipher, we can use a frequency analysis to help identify the most recurring letters in a cipher text and hence make hypothesis of what these letters have been encoded as (e.g. E, T, A, O, etc). This will help us decrypt some of the letters in the text. We can then recognise patterns/words in the partly decoded text to identify more substitutions.";
+        String encrypted = caesarCipher.encrypt(message,12);
+        System.out.println(encrypted);
+        String decrypted = caesarCipher.decrypt(encrypted);
+        System.out.println(decrypted);
     }
 }
-
