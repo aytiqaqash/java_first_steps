58bd66737aa50d48f052864a94d2276768d377d7
aytiqaqash <aytiqaqash@gmail.com>
2022-03-31 01:43:14 -0400

    Added no key decryption

--- src/CaesarCipher/CaesarCipherOne.java
+++ src/CaesarCipher/CaesarCipherOne.java
@@ -56,10 +56,61 @@ public class CaesarCipherOne {
         return encrypt(encrypted);
     }
 
+    // getKey istifadə edəcək
+    public int[] countLetters(String message) {
+        // alphabet
+        String alph = "abcdefghijklmnopqrstuvwxyz"; // kiçik hərflərdən ibarət əlifba
+        // array for each char of alphabet
+        int[] counts = new int[26]; // hər hərfin qarşımıza çıxmasını burada saxlayacağıq
+        // Go through the message chars and add counts for that char
+        for (int k = 0; k < message.length(); k++) {
+            char ch = Character.toLowerCase(message.charAt(k)); // d
+            int dex = alph.indexOf(ch); // 3
+            if (dex != -1) {
+                counts[dex] += 1; // indexin dəyərini bir dənə artır  3 => +1
+            }
+        }
+        return counts;
+    }
+
+    // Most Frequent char index
+    public int maxIndex(int[] vals) {
+        int maxDex = 0;
+        for (int k = 0; k < vals.length; k++) {
+            if (vals[k] >= vals[maxDex]) {
+                maxDex = k;
+            }
+        }
+        return maxDex; // maksimal dəyəri olan index
+    }
+
+    public int getKey(String encrypted){
+        // get the Frequency of the letters 26
+        int[] freqs = countLetters(encrypted); // hər hərfdən neçə dənədir?!
+        int maxDex = maxIndex(freqs); // maksimal dəyəri olan index
+        int dkey = maxDex - 4;
+        if (maxDex < 4) {
+            dkey = 26 - (4 - maxDex); // original açar 26 - 25
+        }
+        return 26 - dkey; // deşifrələmək üçün istifadə oluna biləcək açarı geri qaytarırıq.
+    }
+
+    // Decryption Without Keys
+    public String decryptNoKey(String encrypted) {
+        // create an CaesarCipher object
+        int dkey = getKey(encrypted);
+        CaesarCipherOne cc = new CaesarCipherOne(dkey);
+
+        return cc.encrypt(encrypted); // 17 | 26 - 17
+    }
+
+
     public static void main(String[] args) {
         CaesarCipherOne cco = new CaesarCipherOne(17);
-        String encrypted = cco.encrypt("Test Message");
+        String originalMessage = "Test me one encryption";
+        String encrypted = cco.encrypt(originalMessage);
         String decrypted = cco.decrypt(encrypted);
-        System.out.println("Encrypt: " + encrypted + " | Decrypt: " + decrypted);
+        String decryptedNoKey = cco.decryptNoKey(encrypted);
+        System.out.println("Encrypt: " + encrypted + " | Decrypt: " + decrypted + " | NoKey: " + decryptedNoKey);
     }
 }
