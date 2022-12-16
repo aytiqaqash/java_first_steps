8e09684d7b4e0948371d76ed6401143c5713f509
aytiqaqash <aytiqaqash@gmail.com>
2022-12-16 00:57:32 -0500

    W3School Try Yourself private variable
    Description: Wondered to know if I put default value for the private variable in Person class will I be able to change or get it through the set and get methods.

---
+++ src/W3school/Main.java
@@ -0,0 +1,9 @@
+package W3school;
+
+public class Main {
+    public static void main(String[] args) {
+        Person myObj = new Person();
+//        myObj.setName("John");
+        System.out.println(myObj.getName());
+    }
+}
---
+++ src/W3school/Person.java
@@ -0,0 +1,15 @@
+package W3school;
+
+public class Person {
+    private String name="Ziya";
+
+    // Getter
+    public String getName() {
+        return name;
+    }
+
+    // Setter
+    public void setName(String newName) {
+        this.name = newName;
+    }
+}
